caafcf4e4087efb054dbbcbb1c25e4b357c9d16a
Artem <artemisuzl@gmail.com>
2021-09-10 21:35:01 +0300

    Состояние интернета переписал на LiveData

--- app/src/main/java/com/uzlov/translator/view/main/MainInteractor.kt
+++ app/src/main/java/com/uzlov/translator/view/main/MainInteractor.kt
@@ -5,7 +5,6 @@ import com.uzlov.translator.model.data.WordModel
 import com.uzlov.translator.model.net.INetworkStatus
 import com.uzlov.translator.model.repository.Repository
 import com.uzlov.translator.presenter.Interactor
-import io.reactivex.Observable
 
 class MainInteractor(
     private val remoteRepository: Repository<List<WordModel>>,
@@ -13,12 +12,12 @@ class MainInteractor(
     private val networkStatus: INetworkStatus
 ) : Interactor<AppState> {
 
-    override fun getData(word: String): Observable<AppState> {
-        val isOnline = networkStatus.isOnlineSingle().blockingGet()
-        if (isOnline) {
-            return remoteRepository.getData(word).map { AppState.Success(it) }
+    override suspend fun getData(word: String): AppState {
+        val isOnline = networkStatus.observeSateNetwork().value ?: false
+        return if (isOnline) {
+            AppState.Success(remoteRepository.getData(word))
         } else {
-            return localRepository.getData(word).map { AppState.Success(it) }
+            AppState.Success(localRepository.getData(word))
         }
     }
 }
